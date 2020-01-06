0b2b3342148d29c4918cf37e7af9ccd8fd47885a
Marek Sagan <msagan@MacBook-Pro-Marek.local>
2020-01-06 13:26:39 +0100

    Updates

---
+++ precache-manifest.1e9ca0c53c4f0bd73ac2e6c78b87dcd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c17423aabf1893640742799cdb5e78",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c7706d3a3187f03d2e5c",
+    "url": "/monsters-rolodex/static/css/main.c16c14de.chunk.css"
+  },
+  {
+    "revision": "12ed7e1fae371b4cb936",
+    "url": "/monsters-rolodex/static/js/2.e494a0c8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e494a0c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7706d3a3187f03d2e5c",
+    "url": "/monsters-rolodex/static/js/main.6e27c0f6.chunk.js"
+  },
+  {
+    "revision": "76dff06d2def61fa221b",
+    "url": "/monsters-rolodex/static/js/runtime-main.0d1dab6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
